7f341517231285ad97365312c6274b46df6400a1
Firat Eski <frat888@gmail.com>
2020-04-30 12:28:41 +0300

    Implement harry routes

--- routes/harry.php
+++ routes/harry.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function (Request $request) {
-    return $request->user();
-});
+Route::get('/characters', 'CharacterController@index');
+Route::get('/characters/{characterId}', 'CharacterController@show');
